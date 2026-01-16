7ad53c181b7d48b9e792e0eef85cdda4456e92ba
Siddharth Baleja <siddharthbaleja31@gmail.com>
2026-01-16 23:24:26 +0530

    Changes

--- src/main/java/com/example/cicdadvancedpipeline/CicdadvancedpipelineApplication.java
+++ src/main/java/com/example/cicdadvancedpipeline/CicdadvancedpipelineApplication.java
@@ -3,10 +3,12 @@ package com.example.cicdadvancedpipeline;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @SpringBootApplication
+@RestController
 public class CicdadvancedpipelineApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/example/cicdadvancedpipeline/CicdadvancedpipelineApplicationTests.java
+++ src/test/java/com/example/cicdadvancedpipeline/CicdadvancedpipelineApplicationTests.java
@@ -3,11 +3,18 @@ package com.example.cicdadvancedpipeline;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
 class CicdadvancedpipelineApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+    @Test
+    void testAppLogic() {
+        String status = "UP";
+        assertNotNull(status, "The app status should not be null");
     }
 
 }
